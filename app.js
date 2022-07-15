169c0a89a3003d5e4ec0d44b96379d3e0141a644
Амир Керимов <amir@MacBook-Pro-Amir.local>
2022-07-15 04:24:35 +0300

    fix: Fix bag with dlopen

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
@@ -10,14 +11,18 @@ const limiter = require('./limiter.config');
 const app = express();
 const { PORT = 3000, DATA_BASE = 'mongodb://localhost:27017/movies-explorer-db' } = process.env;
 
-mongoose.connect(DATA_BASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  autoIndex: true,
-});
+// mongoose.connect(DATA_BASE, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   autoIndex: true,
+// });
 
 app.use(requestLogger);
 app.use(limiter);
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}))
 app.use(helmet());
 app.use(express.json());
 app.use(router);
