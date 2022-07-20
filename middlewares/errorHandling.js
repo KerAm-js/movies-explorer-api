10f79550a86b1fdc1b5449c8d3ba204d4037f352
Амир Керимов <amir@MacBook-Pro-Amir.local>
2022-07-20 10:25:07 +0300

    delete console logs in error handling

--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -1,8 +1,4 @@
 module.exports = (err, req, res, next) => {
-  console.log(err);
-  console.log(err.statusCode);
-  console.log(err.code);
-  console.log(err.name);
   if (err.statusCode) {
     res.status(err.statusCode).send({ message: err.message });
     return;
